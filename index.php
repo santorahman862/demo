c6fd694fa0e8f7e72be09dbef53bb44fae5612d9
santorahman862 <santorahman862@gmail.com>
2020-02-24 21:09:04 +0600

    add all

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>new project</title>
+	<link rel="stylesheet" href="style.css">
+	
+	
+</head>
+<body>
+<div class="title">
+
+	<h1>National Institute Of Engineering & Technology</h1>
+	</div>
+		<div class="nav-bar">
+		<strong>Language: </strong>
+		<select>
+			<option>English</option>
+			<option>Bangla</option>
+			<option>Hindi</option>
+
+		</select>
+		<ul>
+			<li><a href="login.php">Log in</a></li>
+			<li><a href="#">Log out</a></li>
+		</ul>
+	</div>
+	<br>
+	
+	<br>
+	<div class="main-nav">
+		<ul>
+			<li class='active'><a href="#"><i class="fa fa-home"></i> Home</a></li>
+			<li><a href="#"><i class="fa fa-home"></i>FAQ</a></li>
+			<li><a href="#"><i class="fa fa-home"></i>About us</a></li>
+			<li><a href="#"><i class="fa fa-home"></i>Department</a>
+			<div class="sub-menu">
+				<ul>
+					<li><a href="#">computer</a></li>
+					<li><a href="#">electrical</a></li>
+					<li><a href="#">textile</a></li>
+					<li><a href="#">civil</a></li>
+					<li><a href="#">gdpm</a></li>
+					<li><a href="#">marine</a></li>
+					
+				</ul>
+
+			</div>
+		</li>
+			<li><a href="#"><i class="fa fa-home"></i>Contact</a></li>
+
+		</ul>
+	</div>
+
+	<div class="ma">
+		<h1><marquee> wellcome!</marquee></h1>
+		</div>
+		
+		<div class="row">
+			
+
+		<div class="row5">
+		<h2>OUR SURVICES</h2>
+		<p> We providing good survices in our campus . We have computer lab in good condition. jhhkdsd djsjdjj hddjhsdj ffhfweurufdk fdjhfashe</p>
+		</div>
+		
+			
+		<div class="row3">
+			<h2>ttttt</h2>
+			<p> We providing good survices in our campus . We have computer lab in good condition. jhhkdsd djsjdjj hddjhsdj ffhfweurufdk fdjhfashe</p>
+		</div>
+		<div class="row4">
+			<h2>ttttt</h2>
+			<p> We providing good survices in our campus . We have computer lab in good condition. jhhkdsd djsjdjj hddjhsdj ffhfweurufdk fdjhfashe</p>
+		</div>
+		</div>
+		<div class="our">
+		<h1>our team</h1>
+		</div>
+		<div class="rown">
+		
+		<div class="id">
+		<img src="img/BeautyPlus_20190331151352606_save.jpg">
+		<div class="o">
+			<h2>ttttt</h2>
+			<p> We providing good survices in our campus . We have computer lab in good condition. jhhkdsd djsjdjj hddjhsdj ffhfweurufdk fdjhfashe</p>
+			</div>
+			</div>
+		<div class="id">
+		<img src="img/BeautyPlus_20190331151352606_save.jpg">
+		<div class="o">
+			<h2>ttttt</h2>
+			<p> We providing good survices in our campus . We have computer lab in good condition. jhhkdsd djsjdjj hddjhsdj ffhfweurufdk fdjhfashe</p>
+			</div>
+			</div>
+		<div class="id">
+		<img src="img/BeautyPlus_20190331151352606_save.jpg">
+		<div class="o">
+			<h2>ttttt</h2>
+			<p> We providing good survices in our campus . We have computer lab in good condition. jhhkdsd djsjdjj hddjhsdj ffhfweurufdk fdjhfashe</p>
+			</div>
+		</div>
+	
+
+	</div>
+</div>
+<br><br><br>
+
+<!-- Department -->
+
+<section id='department'>
+	<div class="row9">
+		<h1>Department</h1>
+	</div>
+</section>
+<div class="body">
+
+</div>
+
+
+</body>
+</html>
